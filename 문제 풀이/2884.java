4ee2c30e03038592054076049fbc2af26f19cc8b
OhHaneol <62991586+OhHaneol@users.noreply.github.com>
2020-09-07 11:59:08 +0900

    알람시계

---
+++ 문제 풀이/2884.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int H=sc.nextInt();
+		int M=sc.nextInt();
+		
+		
+		if(M>45||M==45) {
+			M-=45;
+			System.out.println(H + " " + M);
+		}
+		else {
+			if(H==0) {
+				H=23;
+			}
+			else {
+				--H;
+			}
+			M=60-45+M;
+			System.out.println(H + " " + M);
+		}
+		
+	}
+
+}
