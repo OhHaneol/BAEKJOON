453e89956506f2c61e28d172a4334cb6886492e1
OhHaneol <62991586+OhHaneol@users.noreply.github.com>
2020-09-07 18:18:52 +0900

    예쁜 A+B_2

---
+++ 문제 풀이/3단계/11022.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+   public static void main(String[] args) throws IOException{
+	   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	   int T = Integer.parseInt(br.readLine());
+	   
+	   BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	   
+	   for(int i=0;i<T;i++) {
+		   int A, B;
+		   int x=i+1;
+		   String s = br.readLine();
+		   StringTokenizer st = new StringTokenizer(s);
+		   A = Integer.parseInt(st.nextToken());
+		   B = Integer.parseInt(st.nextToken());
+		   
+		   int sum=A+B;
+		   
+		   System.out.println("Case #" + x + ": " + A + " + " + B + " = " + sum);
+		   bw.newLine();
+	   }
+	   br.close();
+	   bw.flush();
+	   bw.close();
+   }
+}
