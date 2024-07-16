9f9fd853c349e085b67b10327a1a0629736ef1ac
Haneol Oh <otyvs1109@hs.ac.kr>
2024-07-17 00:14:27 +0900

    Update 11725.java

--- DFS/11725.java
+++ DFS/11725.java
@@ -1,54 +1,47 @@
-import java.io.*;  
-import java.util.*;  
-  
-public class Main {  
-  
-    public static void main(String[] args) throws IOException {  
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  
-        int n = Integer.parseInt(br.readLine());  
-        // 각 노드의 부모 노드를 저장하는 배열  
-        int[] parent = new int[n + 1];  
-        // 연결 리스트 배열  
-        ArrayList<Integer>[] adj = new ArrayList[n + 1];  
-        for (int i = 1; i <= n; i++) {  
-            adj[i] = new ArrayList<>();  
-        }  
-        // 방문 체크 배열  
-        boolean[] visited = new boolean[n + 1];  
-        StringTokenizer st;  
-        for (int i = 1; i < n; i++) {  
-            st = new StringTokenizer(br.readLine());  
-            int a = Integer.parseInt(st.nextToken());  
-            int b = Integer.parseInt(st.nextToken());  
-            adj[a].add(b);  
-            adj[b].add(a);  
-        }  
-  
-        // BFS를 위한 큐  
-        Queue<Integer> queue = new LinkedList<>();  
-        // 루트 노드인 1을 큐에 넣고 방문 처리  
-        queue.add(1);  
-        visited[1] = true;  
-        // BFS   
-while (!queue.isEmpty()) {  
-            int cur = queue.poll();  
-            // 현재 노드에 연결된 다음 노드들을 탐색  
-            for (int next : adj[cur]) {  
-                // 방문했던 노드라면 건너뛴다  
-                if (visited[next]) {  
-                    continue;  
-                }  
-                // 다음 노드를 방문 처리하고 큐에 넣는다.  
-                visited[next] = true;  
-                queue.add(next);  
-                // 다음 노드의 부모가 현재 노드임을 저장한다.  
-                parent[next] = cur;  
-            }  
-        }  
-  
-        // 부모 노드를 출력한다.  
-        for (int i = 2; i <= n; i++) {  
-            System.out.println(parent[i]);  
-        }  
-    }  
+package algorithm;
+
+import java.util.*;
+
+class Main {
+    static int n;   // 노드 개수 : 1~n
+    static int[] parent;
+    static boolean[] visited;    // 부모 노드를 저장하는 배열
+    static List<List<Integer>> adjList;
+
+    public static void findParent(int currentNode) {
+        visited[currentNode] = true;
+        for (int adjacent : adjList.get(currentNode)) {
+            if (!visited[adjacent]) {   // 인접 노드가 방문되지 않았다면(자식 노드라면)
+                parent[adjacent] = currentNode; // 인접 노드의 부모 노드를 현재 노드로 설정
+                findParent(adjacent);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        // 인접 리스트 만들기
+        adjList = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            adjList.add(new ArrayList<Integer>());
+        }
+        for (int i = 0; i < n - 1; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            adjList.get(a).add(b);
+            adjList.get(b).add(a);
+        }
+
+        // 부모 노드를 찾기
+        parent = new int[n + 1];
+        visited = new boolean[n + 1];
+        findParent(1);
+
+        // 부모 노드 출력
+        for (int i = 2; i <= n; i++) {
+            System.out.println(parent[i]);
+        }
+    }
 }
