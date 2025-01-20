a05502bfcbeafbdeb4ae4737a97ce9994760c6c1
Haneol Oh <otyvs1109@hs.ac.kr>
2025-01-20 23:10:23 +0900

    Update 선택정렬.java

--- 정렬/2750 수 정렬하기/선택정렬.java
+++ 정렬/2750 수 정렬하기/선택정렬.java
@@ -1,36 +1,34 @@
-package algorithm;
-
-
 import java.io.*;
 
 class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
 
         int[] numbers = new int[n];
-
         for(int i = 0; i < numbers.length; i++) {
           numbers[i] = Integer.parseInt(br.readLine());
-
         }
 
-        for(int i = 0; i < numbers.length; i++) {
+        for(int i = 0; i < numbers.length - 1; i++) {
             for(int j = i + 1; j < numbers.length; j++) {
-              if(numbers[i] > numbers[j]){
-                  int temp = numbers[i];
-                  numbers[i] = numbers[j];
-                  numbers[j] = temp;
-              }
+
+                if(numbers[i] > numbers[j]) {
+                    // 값 교환
+                    int temp = numbers[j];
+                    numbers[j] = numbers[i];
+                    numbers[i] = temp;
+                }
             }
         }
 
         for (int number : numbers) {
-
-            System.out.println(number);
+            sb.append(number).append("\n");
         }
+
+        System.out.println(sb);
   }
 }
