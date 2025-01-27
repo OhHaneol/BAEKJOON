9129f4a56fd152517aadbd77b08b63c591c8b69a
Haneol Oh <otyvs1109@hs.ac.kr>
2025-01-27 23:03:40 +0900

    Update 15649.java

--- DFS/15649/15649.java
+++ DFS/15649/15649.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class boj_15649 {
+public class solution {
     static StringBuilder sb = new StringBuilder();
     static int n, m;
     static boolean[] visit;
